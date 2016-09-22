eab6f957c44b0ddca74bc7fa52b8b70b31600b37
Mindaugas Mažalskis <mindaugas.mazalskis@adform.com>
2016-09-22 16:21:19 +0300

    resolve country code from user IP

--- web/src/Web/Controllers/HomeController.cs
+++ web/src/Web/Controllers/HomeController.cs
@@ -1,12 +1,77 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
 
-namespace Web.Controllers
+namespace AusrytPhoto.WEB.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfigurationRoot _configuration;
+        private static readonly HttpClient HttpClient;
+
+        public HomeController(IConfigurationRoot configuration)
+        {
+            _configuration = configuration;
+        }
+
+        static HomeController()
+        {
+            HttpClient = new HttpClient();
+        }
+
         public IActionResult Index()
         {
+            if (!Request.Cookies.ContainsKey("lang") || string.IsNullOrEmpty(Request.Cookies["lang"]))
+            {
+                Response.Cookies.Append("lang", GetUserContryCode());
+            }
+
             return View();
+        }
+
+
+        private string GetUserContryCode()
+        {
+            string countryCode = string.Empty;
+
+            try
+            {
+                var stringResponse = HttpClient.GetAsync($"{_configuration["ApiUrls:locationAPI"]}{ResolveIp()}").Result.Content.ReadAsStringAsync().Result;
+                var response = JsonConvert.DeserializeObject<JObject>(stringResponse);
+
+                countryCode = response.GetValue("countryCode").ToString();
+
+                switch (countryCode)
+                {
+                    case "LT":
+                        break;
+                    default:
+                        countryCode = "EN";
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
+
+            return countryCode;
+        }
+
+        private string ResolveIp()
+        {
+            var ip = HttpContext.Connection.RemoteIpAddress.ToString();
+
+            //localhost matters only when call from same machine.
+            if (ip == "::1")
+            {
+                ip = string.Empty;
+            }
+
+            return ip;
         }
     }
 }
--- web/src/Web/Startup.cs
+++ web/src/Web/Startup.cs
@@ -25,6 +25,8 @@ namespace Web
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddSingleton(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
