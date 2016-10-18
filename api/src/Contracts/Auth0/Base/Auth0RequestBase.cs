25f5ffb37808869c718a8c33a4c5410ac4526a3d
Mindaugas Mažalskis <mindaugas.mazalskis@adform.com>
2016-10-18 22:33:26 +0300

    contracts

---
+++ api/src/Contracts/Auth0/Base/Auth0RequestBase.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace JumperCables.Contracts.Auth0.Base
+{
+    public class Auth0RequestBase
+    {
+        [JsonProperty("client_id")]
+        public string ClientId { get; set; }
+
+        public string Connection { get; set; }
+    }
+}
---
+++ api/src/Contracts/Auth0/Login/LoginRequest.cs
@@ -0,0 +1,22 @@
+﻿using JumperCables.Contracts.Auth0.Base;
+using Newtonsoft.Json;
+
+namespace JumperCables.Contracts.Auth0.Login
+{
+    public class LoginRequest : Auth0RequestBase
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        [JsonProperty("id_token")]
+        public string TokenId { get; set; }
+
+        [JsonProperty("grant_type")]
+        public string GrantType { get; set; }
+
+        public string Scope { get; set; }
+
+        public string Device { get; set; }
+    }
+}
